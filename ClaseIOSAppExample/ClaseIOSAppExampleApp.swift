cc3e595b1b21af2f698718ff4152f80cdc643465
Antonio <mattct2393@gmail.com>
2023-04-28 11:41:59 -0400

    Initial Commit

---
+++ ClaseIOSAppExample/ClaseIOSAppExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaseIOSAppExampleApp.swift
+//  ClaseIOSAppExample
+//
+//  Created by Melvin Antonio Callisaya Tiñini on 28/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ClaseIOSAppExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClaseIOSAppExample/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ClaseIOSAppExample
+//
+//  Created by Melvin Antonio Callisaya Tiñini on 28/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
